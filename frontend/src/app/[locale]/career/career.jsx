83d490ce0ccaf829d73e1877bd78e2c340cb152f
MaviyomWeb <156904043+MaviyomWeb@users.noreply.github.com>
2025-05-07 12:06:00 +0530

    Update career.jsx

--- frontend/src/app/[locale]/career/career.jsx
+++ frontend/src/app/[locale]/career/career.jsx
@@ -175,7 +175,7 @@ const CareerItem = ({ careerKey, t, messages }) => {
             <textarea
               name="message"
               placeholder="About Yourself"
-              className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 md:text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-500 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+              className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 md:text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
               onChange={handleChange}
             />
 

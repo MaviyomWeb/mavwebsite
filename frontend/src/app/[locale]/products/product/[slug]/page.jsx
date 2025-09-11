c9a3254d93a6fd23a642daed9f5b6c207cc24dd4
Rajnandni Jaiswal <rajnandnijaiswal101@gmail.com>
2025-09-11 15:12:00 +0530

    image change

--- frontend/src/app/[locale]/products/product/[slug]/page.jsx
+++ frontend/src/app/[locale]/products/product/[slug]/page.jsx
@@ -6,8 +6,7 @@ import ProductDetails from "./components/ProductDetails";
 
 const fetchProductsDetails = async (slug) => {
   try {
-    // Use the full API URL from env
-    const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/products?populate=*&filters[slug][$eq]=${slug}`;
+    const apiUrl = `/api/products?populate=*&filters[slug][$eq]=${slug}`;
 
     const product = await fetchDataFromApi(apiUrl);
 
@@ -22,29 +21,34 @@ const fetchProductsDetails = async (slug) => {
 };
 
 const Product = async ({ params }) => {
+
   const { locale, slug } = params;
 
   const res = await fetchProductsDetails(slug);
 
-  // Pick localized version if not English
   const product =
     locale === "en"
       ? res?.data[0]
-      : res?.data[0]?.localizations?.[0];
+      : res?.data[0]?.localizations[0];
 
   const productAllDetails = res?.data[0];
+  
+
 
   return (
-    <div className="relative isolate z-0 pb-10 md:pb-20 mt-10 pt-10">
-      <Container>
-        <ProductDetails
-          data={product}
-          locale={locale}
-          productAllDetails={productAllDetails}
-        />
-      </Container>
+    <div className="relative isolate z-0 pb-10 md:pb-20  mt-10 pt-10">
+      <>
+        <Container>
+          <ProductDetails
+            data={product}
+            locale={locale}
+            productAllDetails={productAllDetails}
+          />
+        </Container>
+      </>
 
-      {/* VTOL Section */}
+      {/* VTOL */}
+
       {/* {productAllDetails?.payload_image && (
         <div className="content md:w-9/12 md:mx-auto mt-10">
           <Payload
